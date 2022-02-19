e61f5358e137d7216474a208b622f13d44312457
Gabriel Guesser <gdguesser@gmail.com>
2022-02-19 18:10:54 -0300

    creating hero header view

---
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,33 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by Gabriel Guesser on 19/02/22.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
