852c86c62d3574a9022fbedf9a119dbd76f4b651
Gabriel Guesser <gdguesser@gmail.com>
2022-02-20 22:45:34 -0300

    creating Movie model to use to decode the tmdb api response

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -24,7 +24,7 @@ class APICaller {
         }
         
         do {
-            let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+            let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
             print(results)
         } catch {
             print(error.localizedDescription)
