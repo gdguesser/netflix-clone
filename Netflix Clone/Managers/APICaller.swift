b88a61dc59f8d3c3f84f711ae5c5999ee033d132
Gabriel Guesser <gdguesser@gmail.com>
2022-02-20 22:52:12 -0300

    minor adjusts

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -12,10 +12,14 @@ struct Constants {
     static let BASE_URL = "https://api.themoviedb.org"
 }
 
+enum APIError: Error {
+    case failedToGetData
+}
+
 class APICaller {
     static let shared = APICaller()
     
-    func getTrendingMovies(completion: @escaping (String) -> Void) {
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
         guard let url = URL(string: "\(Constants.BASE_URL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
                 
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
@@ -25,9 +29,10 @@ class APICaller {
         
         do {
             let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
-            print(results)
+            completion(.success(results.results))
+            
         } catch {
-            print(error.localizedDescription)
+            completion(.failure(error))
         }
         }
         task.resume()
